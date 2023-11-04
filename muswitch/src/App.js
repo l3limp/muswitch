ce436660e5bb56f702db6ae9f5ab38e588c38247
l3limp <kakarvardaan@gmail.com>
2023-11-05 01:06:18 +0530

    frontend is L

--- muswitch/src/App.js
+++ muswitch/src/App.js
@@ -125,92 +125,111 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TextField
-          font="small"
-          id="filled-basic"
-          label="Link of the playlist to be converted"
-          variant="filled"
-          value={oldPlaylistURL}
-          onChange={(e) => {
-            setOldPlaylistURL(e.target.value);
-          }}
-        />
-        <TextField
-          id="filled-basic"
-          label="New playlist name? (no spaces)"
-          variant="filled"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <div className="Hor-Row">
-          <Box sx={{ minWidth: 160 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">
-                Convert From
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={convertFrom}
-                label="Convert From"
-                onChange={handleFromChange}
-              >
-                <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
-                <MenuItem value="Spotify">Spotify</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <Box sx={{ minWidth: 160 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Convert To</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={convertTo}
-                label="Convert To"
-                onChange={handleToChange}
-              >
-                <MenuItem value="Spotify">Spotify</MenuItem>
-                <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
+        <div className="Outer">
+          <div className="Banner"></div>
+          <div className="Body">
+            <div className="Upper-Body">
+              <TextField
+                font="small"
+                id="filled-basic"
+                label="Link of the playlist to be converted"
+                style={{ width: "60vb" }}
+                value={oldPlaylistURL}
+                onChange={(e) => {
+                  setOldPlaylistURL(e.target.value);
+                }}
+              />
+              <TextField
+                id="filled-basic"
+                label="New playlist name? (no spaces)"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="Lower-Body">
+              <Box sx={{ minWidth: 160 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Convert From
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={convertFrom}
+                    label="Convert From"
+                    onChange={handleFromChange}
+                  >
+                    <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+                    <MenuItem value="Spotify">Spotify</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <div style={{ width: "20px" }}></div>
+              <Box sx={{ minWidth: 160 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Convert To
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={convertTo}
+                    label="Convert To"
+                    onChange={handleToChange}
+                  >
+                    <MenuItem value="Spotify">Spotify</MenuItem>
+                    <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+            <div className="Lower-L-Body" >
+              <div className="Warn">
+                {bug && (
+                  <p style={{ color: "red", fontSize: "16px" }}>
+                    To and From cannot be same
+                  </p>
+                )}
+                {isNotValidURL && (
+                  <p style={{ color: "red", fontSize: "16px" }}>
+                    Invalid playlist link
+                  </p>
+                )}
+                {doesURLNotMatch && (
+                  <p style={{ color: "red", fontSize: "16px" }}>
+                    The given URL does not match the source chosen
+                  </p>
+                )}
+                {notFilled && (
+                  <p style={{ color: "red", fontSize: "16px" }}>
+                    The fields cannot be empty
+                  </p>
+                )}
+              </div>
+              <div className="Butt">
+                <Button
+                  variant="contained"
+                  onClick={handleClick}
+                  style={{ width: "20vb" }}
+                >
+                  Go?
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <Button variant="contained" onClick={handleClick}>
-          Go?
-        </Button>
-
-        {bug && (
-          <p style={{ color: "red", fontSize: "16px" }}>
-            To and From cannot be same
-          </p>
-        )}
-        {isNotValidURL && (
-          <p style={{ color: "red", fontSize: "16px" }}>
-            Invalid playlist link
-          </p>
-        )}
-        {doesURLNotMatch && (
-          <p style={{ color: "red", fontSize: "16px" }}>
-            The given URL does not match the source chosen
-          </p>
-        )}
-        {notFilled && (
-          <p style={{ color: "red", fontSize: "16px" }}>
-            The fields cannot be empty
-          </p>
-        )}
-
-{returnURL && (
+        {returnURL && (
           <p style={{ color: "black", fontSize: "16px" }}>
             You can find your playlist at:
           </p>
         )}
 
-        <a href={returnURL} style={{ color: "blue", fontSize: "16px" }}>{returnURL}</a>
+        <a href={returnURL} style={{ color: "blue", fontSize: "16px" }}>
+          {returnURL}
+        </a>
       </header>
     </div>
   );
