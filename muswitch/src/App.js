3b596232c4e5e62aeb699909249a4cd978a85f76
l3limp <kakarvardaan@gmail.com>
2023-11-05 00:04:43 +0530

    added checks for input

--- muswitch/src/App.js
+++ muswitch/src/App.js
@@ -1,40 +1,105 @@
 import "./App.css";
-import { TextField, Button } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState("dattebayo");
   const [oldPlaylistURL, setOldPlaylistURL] = useState("");
   const [convertFrom, setConvertFrom] = useState("");
   const [convertTo, setConvertTo] = useState("");
   const [returnURL, setReturnURL] = useState("");
+  const [bug, setBug] = useState(false);
+  const [isNotValidURL, setIsNotValidURL] = useState(false);
+  const [doesURLNotMatch, setDoesURLNotMatch] = useState(false);
+  const [notFilled, setNotFilled] = useState(false);
 
   function handleClick() {
-    var from = "";
-    var to = "";
-    if(convertFrom==='Y' || convertFrom ==='y') from = "YouTubeMusic";
-    if(convertTo==='Y' || convertTo ==='y') to = "YouTubeMusic";
-    if(convertFrom==='S' || convertFrom ==='s') from = "Spotify";
-    if(convertTo==='S' || convertTo ==='s') to = "Spotify";
-
-    // convert URL to ID
     let oldPlaylistID = "";
-    if(from === "YouTubeMusic") oldPlaylistID = oldPlaylistURL.substring(34, 68);
-    if(from === "Spotify") oldPlaylistID = oldPlaylistURL.substring(34, 56);
 
-    console.log(from);
-    console.log(to);
+    if (convertFrom === "" || convertTo === "" || oldPlaylistURL === "") {
+      setNotFilled(true);
+      return;
+    }
+    setNotFilled(false);
+
+    if (convertFrom === convertTo) {
+      setBug(true);
+      return;
+    }
+    setBug(false);
+
+    const youtubeMusicPlaylistIdRegex =
+      /\b(https?:\/\/[^/]*\byoutube\.com\/playlist\?list=(.*?)(?:&|$)\b)/i;
+    const ytmMatch = youtubeMusicPlaylistIdRegex.exec(oldPlaylistURL);
+    let ytmLink = true;
+
+    if (ytmMatch) {
+      oldPlaylistID = ytmMatch[2];
+
+      console.log("The playlist ID is:", oldPlaylistID);
+    } else {
+      ytmLink = false;
+      console.log("The string is not a YouTube Music playlist URL.");
+    }
+
+    const spotifyPlaylistIdRegex =
+      /\b(https?:\/\/[^\/]*\bspotify\.com\/playlist\/([^\s?]+)\b)/i;
+    const spotifyMatch = spotifyPlaylistIdRegex.exec(oldPlaylistURL);
+    let spotifyLink = true;
+
+    if (spotifyMatch) {
+      oldPlaylistID = spotifyMatch[2];
+
+      console.log("The playlist ID is:", oldPlaylistID);
+    } else {
+      spotifyLink = false;
+      console.log("The string is not a Spotify playlist URL.");
+    }
+
+    if (!spotifyLink && !ytmLink) {
+      setIsNotValidURL(true);
+      return;
+    }
+
+    setIsNotValidURL(false);
+
+    if (
+      (convertFrom === "Spotify" && !spotifyLink) ||
+      (convertFrom === "YouTubeMusic" && !ytmLink)
+    ) {
+      setDoesURLNotMatch(true);
+      return;
+    }
+    setDoesURLNotMatch(false);
+
+    console.log(convertFrom);
+    console.log(convertTo);
     console.log(oldPlaylistID);
     console.log(name);
 
     var url =
-      "http://localhost:8080/createNew?fromPlatform="+from+"&toPlatform="+to+"&oldPlaylistID="+oldPlaylistID+"&newPlaylistName="+name;
+      "http://localhost:8080/createNew?fromPlatform=" +
+      convertFrom +
+      "&toPlatform=" +
+      convertTo +
+      "&oldPlaylistID=" +
+      oldPlaylistID +
+      "&newPlaylistName=" +
+      name;
 
     axios
       .get(url)
       .then((res) => {
-        setReturnURL("You can find your playlist at: " + res.data['rez']);
+        setReturnURL(res.data["rez"]);
       })
       .catch((error) => {
         console.error(error);
@@ -49,6 +114,13 @@ function App() {
   //       console.error(error);
   //     });
   // }, []);
+
+  const handleFromChange = (event: SelectChangeEvent) => {
+    setConvertFrom(event.target.value);
+  };
+  const handleToChange = (event: SelectChangeEvent) => {
+    setConvertTo(event.target.value);
+  };
 
   return (
     <div className="App">
@@ -72,34 +144,73 @@ function App() {
             setName(e.target.value);
           }}
         />
-        <TextField
-          inputProps={{ maxLength: 1 }}
-          id="filled-basic"
-          label="Convert from"
-          variant="filled"
-          value={convertFrom}
-          onChange={(e) => {
-            setConvertFrom(e.target.value);
-          }}
-        />
-        <TextField
-          inputProps={{ maxLength: 1 }}
-          id="filled-basic"
-          label="Convert to"
-          variant="filled"
-          value={convertTo}
-          onChange={(e) => {
-            setConvertTo(e.target.value);
-          }}
-        />
-
-        <p>type Y for youtube music or S for spotify</p>
+        <div className="Hor-Row">
+          <Box sx={{ minWidth: 160 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Convert From
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={convertFrom}
+                label="Convert From"
+                onChange={handleFromChange}
+              >
+                <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+                <MenuItem value="Spotify">Spotify</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ minWidth: 160 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Convert To</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={convertTo}
+                label="Convert To"
+                onChange={handleToChange}
+              >
+                <MenuItem value="Spotify">Spotify</MenuItem>
+                <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
 
         <Button variant="contained" onClick={handleClick}>
           Go?
         </Button>
 
-        <p>{returnURL}</p>
+        {bug && (
+          <p style={{ color: "red", fontSize: "16px" }}>
+            To and From cannot be same
+          </p>
+        )}
+        {isNotValidURL && (
+          <p style={{ color: "red", fontSize: "16px" }}>
+            Invalid playlist link
+          </p>
+        )}
+        {doesURLNotMatch && (
+          <p style={{ color: "red", fontSize: "16px" }}>
+            The given URL does not match the source chosen
+          </p>
+        )}
+        {notFilled && (
+          <p style={{ color: "red", fontSize: "16px" }}>
+            The fields cannot be empty
+          </p>
+        )}
+
+{returnURL && (
+          <p style={{ color: "black", fontSize: "16px" }}>
+            You can find your playlist at:
+          </p>
+        )}
+
+        <a href={returnURL} style={{ color: "blue", fontSize: "16px" }}>{returnURL}</a>
       </header>
     </div>
   );
