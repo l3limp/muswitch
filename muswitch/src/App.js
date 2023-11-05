2dcd91914b86a6e76e911e2607a1557deabd465e
l3limp <kakarvardaan@gmail.com>
2023-11-05 16:16:27 +0530

    fin

--- muswitch/src/App.js
+++ muswitch/src/App.js
@@ -7,6 +7,7 @@ import {
   Select,
   InputLabel,
   FormControl,
+  CircularProgress,
 } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
@@ -21,6 +22,8 @@ function App() {
   const [isNotValidURL, setIsNotValidURL] = useState(false);
   const [doesURLNotMatch, setDoesURLNotMatch] = useState(false);
   const [notFilled, setNotFilled] = useState(false);
+  const [bodyCheck, setBodyCheck] = useState(true);
+  const [circCheck, setCircCheck] = useState(true);
 
   function handleClick() {
     let oldPlaylistID = "";
@@ -81,6 +84,10 @@ function App() {
     }
     setDoesURLNotMatch(false);
 
+    var fetchTxt = document.getElementById("toggleBoldTextFetch");
+    fetchTxt.classList.toggle("bold");
+    setBodyCheck(false);
+
     console.log(convertFrom);
     console.log(convertTo);
     console.log(oldPlaylistID);
@@ -100,21 +107,18 @@ function App() {
       .get(url)
       .then((res) => {
         setReturnURL(res.data["rez"]);
+        setCircCheck(false);
+        var listenTxt = document.getElementById("toggleBoldTextListen");
+        listenTxt.classList.toggle("bold");
       })
       .catch((error) => {
         console.error(error);
+        setReturnURL("Error :(");
+        setCircCheck(false);
+        var listenTxt = document.getElementById("toggleBoldTextListen");
+        listenTxt.classList.toggle("bold");
       });
   }
-  // useEffect(() => {
-  //   axios.get('https://jsonplaceholder.typicode.com/posts')
-  //     .then(response => {
-  //       setReturnURL(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // }, []);
-
   const handleFromChange = (event: SelectChangeEvent) => {
     setConvertFrom(event.target.value);
   };
@@ -126,110 +130,150 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Outer">
-          <div className="Banner"></div>
-          <div className="Body">
-            <div className="Upper-Body">
-              <TextField
-                font="small"
-                id="filled-basic"
-                label="Link of the playlist to be converted"
-                style={{ width: "60vb" }}
-                value={oldPlaylistURL}
-                onChange={(e) => {
-                  setOldPlaylistURL(e.target.value);
-                }}
-              />
-              <TextField
-                id="filled-basic"
-                label="New playlist name? (no spaces)"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-            </div>
-            <div className="Lower-Body">
-              <Box sx={{ minWidth: 160 }}>
-                <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">
-                    Convert From
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={convertFrom}
-                    label="Convert From"
-                    onChange={handleFromChange}
-                  >
-                    <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
-                    <MenuItem value="Spotify">Spotify</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-              <div style={{ width: "20px" }}></div>
-              <Box sx={{ minWidth: 160 }}>
-                <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">
-                    Convert To
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={convertTo}
-                    label="Convert To"
-                    onChange={handleToChange}
-                  >
-                    <MenuItem value="Spotify">Spotify</MenuItem>
-                    <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-            </div>
-            <div className="Lower-L-Body" >
-              <div className="Warn">
-                {bug && (
-                  <p style={{ color: "red", fontSize: "16px" }}>
-                    To and From cannot be same
-                  </p>
-                )}
-                {isNotValidURL && (
-                  <p style={{ color: "red", fontSize: "16px" }}>
-                    Invalid playlist link
-                  </p>
-                )}
-                {doesURLNotMatch && (
-                  <p style={{ color: "red", fontSize: "16px" }}>
-                    The given URL does not match the source chosen
-                  </p>
-                )}
-                {notFilled && (
-                  <p style={{ color: "red", fontSize: "16px" }}>
-                    The fields cannot be empty
-                  </p>
-                )}
+          <div className="Banner">
+            <div className="Stepper">
+              <div className="Stepper-Item">
+                <img
+                  src="https://cdn1.iconfinder.com/data/icons/taxation-7/64/online-registration-form-application-information-512.png"
+                  alt="Logo"
+                  width="30px"
+                />
+                <b>Fill out</b>
               </div>
-              <div className="Butt">
-                <Button
-                  variant="contained"
-                  onClick={handleClick}
-                  style={{ width: "20vb" }}
-                >
-                  Go?
-                </Button>
+              <div className="Stepper-Item">____________</div>
+              <div className="Stepper-Item">
+                <img
+                  src="https://www.iconpacks.net/icons/2/free-paper-plane-icon-2563-thumb.png"
+                  alt="Logo"
+                  width="30px"
+                />
+                <text id="toggleBoldTextFetch">Fetch</text>
+              </div>
+              <div className="Stepper-Item">____________</div>
+              <div className="Stepper-Item">
+                <img
+                  src="https://www.svgrepo.com/show/288598/earphone-headphones.svg"
+                  alt="Logo"
+                  width="30px"
+                />
+                <text id="toggleBoldTextListen">Listen</text>
               </div>
             </div>
           </div>
+
+          <div className="Body">
+            {bodyCheck && (
+              <div>
+                <div className="Upper-Body">
+                  <TextField
+                    font="small"
+                    id="filled-basic"
+                    label="Link of the playlist to be converted"
+                    style={{ width: "60vb" }}
+                    value={oldPlaylistURL}
+                    onChange={(e) => {
+                      setOldPlaylistURL(e.target.value);
+                    }}
+                  />
+                  <TextField
+                    id="filled-basic"
+                    label="New playlist name? (no spaces)"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="Lower-Body">
+                  <Box sx={{ minWidth: 160 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">
+                        Convert From
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={convertFrom}
+                        label="Convert From"
+                        onChange={handleFromChange}
+                      >
+                        <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+                        <MenuItem value="Spotify">Spotify</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <div style={{ width: "20px" }}></div>
+                  <Box sx={{ minWidth: 160 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">
+                        Convert To
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={convertTo}
+                        label="Convert To"
+                        onChange={handleToChange}
+                      >
+                        <MenuItem value="Spotify">Spotify</MenuItem>
+                        <MenuItem value="YouTubeMusic">YouTubeMusic</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </div>
+                <div className="Lower-L-Body">
+                  <div className="Warn">
+                    {bug && (
+                      <p style={{ color: "red", fontSize: "16px" }}>
+                        To and From cannot be same
+                      </p>
+                    )}
+                    {isNotValidURL && (
+                      <p style={{ color: "red", fontSize: "16px" }}>
+                        Invalid playlist link
+                      </p>
+                    )}
+                    {doesURLNotMatch && (
+                      <p style={{ color: "red", fontSize: "16px" }}>
+                        The given URL does not match the source chosen
+                      </p>
+                    )}
+                    {notFilled && (
+                      <p style={{ color: "red", fontSize: "16px" }}>
+                        The fields cannot be empty
+                      </p>
+                    )}
+                  </div>
+                  <div className="Butt">
+                    <Button
+                      variant="contained"
+                      color="success"
+                      onClick={handleClick}
+                      style={{ width: "20vb" }}
+                    >
+                      Go?
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            )}
+            {!bodyCheck && circCheck && (
+              <div className="circ">
+                <CircularProgress color="inherit" />
+              </div>
+            )}
+
+            {returnURL && (
+              <p style={{ color: "black", fontSize: "16px" }}>
+                You can find your playlist at:
+              </p>
+            )}
+
+            <a href={returnURL} style={{ color: "blue", fontSize: "16px" }}>
+              {returnURL}
+            </a>
+          </div>
         </div>
-
-        {returnURL && (
-          <p style={{ color: "black", fontSize: "16px" }}>
-            You can find your playlist at:
-          </p>
-        )}
-
-        <a href={returnURL} style={{ color: "blue", fontSize: "16px" }}>
-          {returnURL}
-        </a>
       </header>
     </div>
   );
